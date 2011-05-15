09d147d3d77d90a585e36fb864bdc6021952fe00
destiroid <sascha.schwaerzler@gmail.com>
2011-05-16 01:43:51 +0200

    Starting with Blog-Tutorial for Rails.
    Step 1 to 7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baezibaer::Application.config.secret_token = 'd8c037fdeee3bc4e438e022f49d1929a16973f64120435fd1c20f917fe156345a93fc55ed1ceca240d79737acebb55b7db9d744540d5d0f8dc97ff1ace58b334'
